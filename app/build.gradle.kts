4a85f734b0f1b0f4daebd90a0eb44059d2a85d73
sim <dango123001@gmail.com>
2025-05-28 14:29:40 +0900

    일정 db저장 기능 구현, 일정을 추가한 날짜에 하이라이트 기능 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation("com.prolificinteractive:material-calendarview:1.4.3")
+
 
     // Navigation Component (추가)
     implementation(libs.navigation.fragment)
